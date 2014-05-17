dd0a19dd5e071e91b3fd5506b3eec51ed88b6bcd
Torben Hansen <derhansen@gmail.com>
2014-05-17 13:49:13 +0200

    [TASK] Removed a comma in SQL file- #2

--- ext_tables.sql
+++ ext_tables.sql
@@ -143,7 +143,7 @@ CREATE TABLE tx_sfeventmgt_domain_model_booking (
 #
 CREATE TABLE tx_sfeventmgt_domain_model_booking (
 
-	event  int(11) unsigned DEFAULT '0' NOT NULL,
+	event  int(11) unsigned DEFAULT '0' NOT NULL
 
 );
 
