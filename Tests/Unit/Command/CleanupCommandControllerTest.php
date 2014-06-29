d6b33f081338a766c0ad9b46f266a70a4f2a82c9
Torben Hansen <derhansen@gmail.com>
2014-06-29 12:23:42 +0200

    [TASK] Added test for cleanupCommand

---
+++ Tests/Unit/Command/CleanupCommandControllerTest.php
@@ -0,0 +1,95 @@
+<?php
+namespace SKYFILLERS\SfEventMgt\Tests\Unit\Command;
+
+	/***************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c) 2014 Torben Hansen <derhansen@gmail.com>, Skyfillers GmbH
+	 *
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 2 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
+
+/**
+ * Test case for class SKYFILLERS\SfEventMgt\Command\CleanupCommandController.
+ */
+class CleanupCommandControllerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/**
+	 * @var \SKYFILLERS\SfEventMgt\Command\CleanupCommandController
+	 */
+	protected $subject = NULL;
+
+	/**
+	 * Setup
+	 *
+	 * @return void
+	 */
+	protected function setUp() {
+		$this->subject = new \SKYFILLERS\SfEventMgt\Command\CleanupCommandController();
+	}
+
+	/**
+	 * Teardown
+	 *
+	 * @return void
+	 */
+	protected function tearDown() {
+		unset($this->subject);
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function cleanupCommandHandlesExpiredRegistrationsAndCleansCacheTest() {
+
+		// Inject configuration and configurationManager
+		$configuration = array(
+			'plugin.' => array(
+				'tx_sfeventmgt.' => array(
+					'settings.' => array(
+						'clearCacheUids' => '1,2,3',
+						'registration.' => array(
+							'deleteExpiredRegistrations' => 0
+						)
+					)
+				)
+			)
+		);
+
+		$configurationManager = $this->getMock('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager',
+			array('getConfiguration'), array(), '', FALSE);
+		$configurationManager->expects($this->once())->method('getConfiguration')->will(
+			$this->returnValue($configuration));
+		$this->inject($this->subject, 'configurationManager', $configurationManager);
+
+		$registrationService = $this->getMock('SKYFILLERS\\SfEventMgt\\Service\\RegistrationService',
+			array('handleExpiredRegistrations'), array(), '', FALSE);
+		$registrationService->expects($this->once())->method('handleExpiredRegistrations')->with(0)->will(
+			$this->returnValue($configuration));
+		$this->inject($this->subject, 'registrationService', $registrationService);
+
+		$cacheService = $this->getMock('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager',
+			array('clearPageCache'), array(), '', FALSE);
+		$cacheService->expects($this->once())->method('clearPageCache')->with(array(1,2,3));
+		$this->inject($this->subject, 'cacheService', $cacheService);
+
+		$this->subject->cleanupCommand();
+	}
+}
