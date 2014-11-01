c03140993a149f393abe901b8099514ac59a358c
Torben Hansen <derhansen@gmail.com>
2014-11-01 13:50:05 +0100

    [TASK] Moved Image to Media Tab - Closes #115

--- Configuration/TCA/tx_sfeventmgt_domain_model_event.php
+++ Configuration/TCA/tx_sfeventmgt_domain_model_event.php
@@ -29,7 +29,7 @@ return array(
 		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, teaser, description, link, top_event, startdate, enddate, enable_registration, max_participants, registration_deadline, price, currency, category, image, registration, location',
 	),
 	'types' => array(
-		'1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title, teaser, description;;;richtext:rte_transform[mode=ts_links], link, top_event, startdate, enddate, location, price, currency, category, image, --div--;LLL:EXT:sf_event_mgt/Resources/Private/Language/locallang_db.xlf:event.tabs.registration, enable_registration, registration_deadline, max_participants, registration, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'),
+		'1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title, teaser, description;;;richtext:rte_transform[mode=ts_links], link, top_event, startdate, enddate, location, price, currency, category, --div--;LLL:EXT:sf_event_mgt/Resources/Private/Language/locallang_db.xlf:event.tabs.media, image, --div--;LLL:EXT:sf_event_mgt/Resources/Private/Language/locallang_db.xlf:event.tabs.registration, enable_registration, registration_deadline, max_participants, registration, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'),
 	),
 	'palettes' => array(
 		'1' => array('showitem' => ''),
