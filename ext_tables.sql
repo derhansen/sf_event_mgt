f2ef27063f52261b6ff67bf3ef7cd617167a2769
Torben Hansen <derhansen@gmail.com>
2016-11-11 15:58:57 +0100

    [BUG] Fixed SQL error in ext_tables.sql #384

--- ext_tables.sql
+++ ext_tables.sql
@@ -142,7 +142,7 @@ CREATE TABLE tx_sfeventmgt_domain_model_customnotificationlog (
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
+	PRIMARY KEY (uid)
 );
 
 #
