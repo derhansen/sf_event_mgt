29d3958b7136f7a619b2b766b7b77d324a6aeef2
Torben Hansen <derhansen@gmail.com>
2017-08-18 19:40:46 +0200

    [TASK] Do not copy registrations when copying an event
    Closes #464

--- Configuration/TCA/tx_sfeventmgt_domain_model_event.php
+++ Configuration/TCA/tx_sfeventmgt_domain_model_event.php
@@ -14,6 +14,7 @@ return [
         'languageField' => 'sys_language_uid',
         'transOrigPointerField' => 'l10n_parent',
         'transOrigDiffSourceField' => 'l10n_diffsource',
+        'setToDefaultOnCopy' => 'registration',
         'delete' => 'deleted',
         'enablecolumns' => [
             'disabled' => 'hidden',
