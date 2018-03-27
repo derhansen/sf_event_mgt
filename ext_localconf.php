230376f8b10054f43d4dd877a9dcaf757f911bea
Torben Hansen <derhansen@gmail.com>
2018-03-27 19:07:13 +0200

    [TASK] Use closure in ext_localconf.php and ext_tables.php
    Closes #525

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,72 +1,72 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'DERHANSEN.' . $_EXTKEY,
-    'Pievent',
-    [
-        'Event' => 'list, detail, calendar, registration, saveRegistration, saveRegistrationResult, confirmRegistration, cancelRegistration, icalDownload, search',
-    ],
-    // non-cacheable actions
-    [
-        'Event' => 'registration, saveRegistration, saveRegistrationResult, confirmRegistration, cancelRegistration, icalDownload, search',
-    ]
-);
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'DERHANSEN.sf_event_mgt',
+        'Pievent',
+        [
+            'Event' => 'list, detail, calendar, registration, saveRegistration, saveRegistrationResult, confirmRegistration, cancelRegistration, icalDownload, search',
+        ],
+        // non-cacheable actions
+        [
+            'Event' => 'registration, saveRegistration, saveRegistrationResult, confirmRegistration, cancelRegistration, icalDownload, search',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'DERHANSEN.' . $_EXTKEY,
-    'Piuserreg',
-    [
-        'UserRegistration' => 'list',
-    ],
-    // non-cacheable actions
-    [
-        'UserRegistration' => 'list',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'DERHANSEN.sf_event_mgt',
+        'Piuserreg',
+        [
+            'UserRegistration' => 'list',
+        ],
+        // non-cacheable actions
+        [
+            'UserRegistration' => 'list',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'DERHANSEN.' . $_EXTKEY,
-    'Pipayment',
-    [
-        'Payment' => 'redirect, success, failure, cancel, notify',
-    ],
-    // non-cacheable actions
-    [
-        'Payment' => 'redirect, success, failure, cancel, notify',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'DERHANSEN.sf_event_mgt',
+        'Pipayment',
+        [
+            'Payment' => 'redirect, success, failure, cancel, notify',
+        ],
+        // non-cacheable actions
+        [
+            'Payment' => 'redirect, success, failure, cancel, notify',
+        ]
+    );
 
-// DataHandler hook
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['DERHANSEN.' . $_EXTKEY] =
-    'DERHANSEN\SfEventMgt\Hooks\DataHandlerHooks';
+    // DataHandler hook
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['DERHANSEN.sf_event_mgt'] =
+        'DERHANSEN\SfEventMgt\Hooks\DataHandlerHooks';
 
-// Page layout hooks to show preview of plugins
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['sfeventmgt_pievent']['event'] =
-    \DERHANSEN\SfEventMgt\Hooks\PageLayoutView::class . '->getEventPluginSummary';
+    // Page layout hooks to show preview of plugins
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['sfeventmgt_pievent']['event'] =
+        \DERHANSEN\SfEventMgt\Hooks\PageLayoutView::class . '->getEventPluginSummary';
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['sfeventmgt_piuserreg']['userreg'] =
-    \DERHANSEN\SfEventMgt\Hooks\PageLayoutView::class . '->getUserRegPluginSummary';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['sfeventmgt_piuserreg']['userreg'] =
+        \DERHANSEN\SfEventMgt\Hooks\PageLayoutView::class . '->getUserRegPluginSummary';
 
-// Register cleanup command
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'DERHANSEN\\SfEventMgt\\Command\\CleanupCommandController';
-}
+    // Register cleanup command
+    if (TYPO3_MODE === 'BE') {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'DERHANSEN\\SfEventMgt\\Command\\CleanupCommandController';
+    }
 
-// Register longitude- and latitude-evaluator for TCA
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['DERHANSEN\\SfEventMgt\\Evaluation\\LongitudeEvaluator'] = 'EXT:sf_event_mgt/Classes/Evaluation/LongitudeEvaluator.php';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['DERHANSEN\\SfEventMgt\\Evaluation\\LatitudeEvaluator'] = 'EXT:sf_event_mgt/Classes/Evaluation/LatitudeEvaluator.php';
+    // Register longitude- and latitude-evaluator for TCA
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['DERHANSEN\\SfEventMgt\\Evaluation\\LongitudeEvaluator'] = 'EXT:sf_event_mgt/Classes/Evaluation/LongitudeEvaluator.php';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['DERHANSEN\\SfEventMgt\\Evaluation\\LatitudeEvaluator'] = 'EXT:sf_event_mgt/Classes/Evaluation/LatitudeEvaluator.php';
 
-// Register default payment methods
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['sf_event_mgt']['paymentMethods'] = [
-    'invoice' => [
-        'class' => 'DERHANSEN\\SfEventMgt\\Payment\\Invoice',
-        'extkey' => 'sf_event_mgt'
-    ],
-    'transfer' => [
-        'class' => 'DERHANSEN\\SfEventMgt\\Payment\\Transfer',
-        'extkey' => 'sf_event_mgt'
-    ]
-];
+    // Register default payment methods
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['sf_event_mgt']['paymentMethods'] = [
+        'invoice' => [
+            'class' => 'DERHANSEN\\SfEventMgt\\Payment\\Invoice',
+            'extkey' => 'sf_event_mgt'
+        ],
+        'transfer' => [
+            'class' => 'DERHANSEN\\SfEventMgt\\Payment\\Transfer',
+            'extkey' => 'sf_event_mgt'
+        ]
+    ];
+});
--- ext_tables.php
+++ ext_tables.php
@@ -1,80 +1,71 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sfeventmgt_domain_model_event',
-    'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_tx_sfeventmgt_domain_model_event.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sfeventmgt_domain_model_event');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sfeventmgt_domain_model_location',
-    'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_tx_sfeventmgt_domain_model_location.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sfeventmgt_domain_model_location');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sfeventmgt_domain_model_organisator',
-    'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_tx_sfeventmgt_domain_model_organisator.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sfeventmgt_domain_model_organisator');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sfeventmgt_domain_model_registration',
-    'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_tx_sfeventmgt_domain_model_registration.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sfeventmgt_domain_model_registration');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sfeventmgt_domain_model_priceoption',
-    'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_tx_sfeventmgt_domain_model_priceoption.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sfeventmgt_domain_model_priceoption');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sfeventmgt_domain_model_speaker',
-    'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_tx_sfeventmgt_domain_model_speaker.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sfeventmgt_domain_model_speaker');
-
-if (TYPO3_MODE === 'BE') {
-    // Register Administration Module
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'DERHANSEN.sf_event_mgt',
-        'web',
-        'tx_sfeventmgt_m1',
-        '',
-        [
-            'Administration' => 'list, export, handleExpiredRegistrations, indexNotify, notify, settingsError',
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/events.svg',
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_modadministration.xlf',
-        ]
-    );
-
-    $icons = [
-        'apps-pagetree-folder-contains-events' => 'events-folder.png',
-        'ext-sfeventmgt-registration-unconfirmed' => 'tx_sfeventmgt_domain_model_registration_unconfirmed.svg',
-        'ext-sfeventmgt-registration-confirmed' => 'tx_sfeventmgt_domain_model_registration_confirmed.svg',
-        'ext-sfeventmgt-event' => 'tx_sfeventmgt_domain_model_event.svg',
-        'ext-sfeventmgt-priceoption' => 'tx_sfeventmgt_domain_model_priceoption.svg',
-        'ext-sfeventmgt-organisator' => 'tx_sfeventmgt_domain_model_organisator.svg',
-        'ext-sfeventmgt-location' => 'tx_sfeventmgt_domain_model_location.svg',
-        'ext-sfeventmgt-speaker' => 'tx_sfeventmgt_domain_model_speaker.svg',
-        'ext-sfeventmgt-registration-field' => 'tx_sfeventmgt_domain_model_registration_field.svg',
-        'ext-sfeventmgt-logfile' => 'logfile.svg',
+call_user_func(function () {
+    /**
+     * Table description files for localization and allowing sf_event_mgt tables on pages of type default
+     */
+    $tables = [
+        'tx_sfeventmgt_domain_model_event',
+        'tx_sfeventmgt_domain_model_location',
+        'tx_sfeventmgt_domain_model_organisator',
+        'tx_sfeventmgt_domain_model_registration',
+        'tx_sfeventmgt_domain_model_priceoption',
+        'tx_sfeventmgt_domain_model_speaker'
     ];
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($icons as $identifier => $path) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:sf_event_mgt/Resources/Public/Icons/' . $path]
+
+    foreach ($tables as $table) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            $table,
+            'EXT:sf_event_mgt/Resources/Private/Language/locallang_csh_' . $table . '.xlf'
         );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages($table);
     }
-}
+
+    if (TYPO3_MODE === 'BE') {
+        /**
+         * Register Administration Module
+         */
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'DERHANSEN.sf_event_mgt',
+            'web',
+            'tx_sfeventmgt_m1',
+            '',
+            [
+                'Administration' => 'list, export, handleExpiredRegistrations, indexNotify, notify, settingsError',
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:sf_event_mgt/Resources/Public/Icons/events.svg',
+                'labels' => 'LLL:EXT:sf_event_mgt/Resources/Private/Language/locallang_modadministration.xlf',
+            ]
+        );
+
+        /**
+         * Register icons
+         */
+        $icons = [
+            'apps-pagetree-folder-contains-events' => 'events-folder.png',
+            'ext-sfeventmgt-registration-unconfirmed' => 'tx_sfeventmgt_domain_model_registration_unconfirmed.svg',
+            'ext-sfeventmgt-registration-confirmed' => 'tx_sfeventmgt_domain_model_registration_confirmed.svg',
+            'ext-sfeventmgt-event' => 'tx_sfeventmgt_domain_model_event.svg',
+            'ext-sfeventmgt-priceoption' => 'tx_sfeventmgt_domain_model_priceoption.svg',
+            'ext-sfeventmgt-organisator' => 'tx_sfeventmgt_domain_model_organisator.svg',
+            'ext-sfeventmgt-location' => 'tx_sfeventmgt_domain_model_location.svg',
+            'ext-sfeventmgt-speaker' => 'tx_sfeventmgt_domain_model_speaker.svg',
+            'ext-sfeventmgt-registration-field' => 'tx_sfeventmgt_domain_model_registration_field.svg',
+            'ext-sfeventmgt-logfile' => 'logfile.svg',
+        ];
+        /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Imaging\IconRegistry::class
+        );
+        foreach ($icons as $identifier => $path) {
+            $iconRegistry->registerIcon(
+                $identifier,
+                \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+                ['source' => 'EXT:sf_event_mgt/Resources/Public/Icons/' . $path]
+            );
+        }
+    }
+});
