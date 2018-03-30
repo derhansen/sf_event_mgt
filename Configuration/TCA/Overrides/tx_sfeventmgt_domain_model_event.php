458cbf285d5f04a3a43918fe50debc7dad2e9cbe
Torben Hansen <derhansen@gmail.com>
2018-03-30 13:35:17 +0200

    [TASK] Small CGL followup for #519

--- Configuration/TCA/Overrides/tx_sfeventmgt_domain_model_event.php
+++ Configuration/TCA/Overrides/tx_sfeventmgt_domain_model_event.php
@@ -12,5 +12,5 @@ defined('TYPO3_MODE') or die();
     ]
 );
 
-# Enable language synchronisation for the category field
+// Enable language synchronisation for the category field
 $GLOBALS['TCA']['tx_sfeventmgt_domain_model_event']['columns']['category']['config']['behaviour']['allowLanguageSynchronization'] = true;
