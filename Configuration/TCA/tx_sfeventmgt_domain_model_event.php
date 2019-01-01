f1d180baa611b1614d17ca79ce1fb22ad0aa687c
Torben Hansen <derhansen@gmail.com>
2019-01-01 06:42:17 +0100

    [TASK] Prevent copy of waitlist registrations
    Refs #590

--- Configuration/TCA/tx_sfeventmgt_domain_model_event.php
+++ Configuration/TCA/tx_sfeventmgt_domain_model_event.php
@@ -15,7 +15,7 @@ return [
         'languageField' => 'sys_language_uid',
         'transOrigPointerField' => 'l10n_parent',
         'transOrigDiffSourceField' => 'l10n_diffsource',
-        'setToDefaultOnCopy' => 'registration',
+        'setToDefaultOnCopy' => 'registration, registration_waitlist',
         'delete' => 'deleted',
         'enablecolumns' => [
             'disabled' => 'hidden',
