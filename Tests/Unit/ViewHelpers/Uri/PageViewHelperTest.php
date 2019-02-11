f86da034d59cc15625fde0bcda3abc347f5dbb88
Torben Hansen <derhansen@gmail.com>
2019-02-11 08:13:34 +0100

    [TASK] Mark PageViewHelperTest as incomplete

--- Tests/Unit/ViewHelpers/Uri/PageViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Uri/PageViewHelperTest.php
@@ -8,12 +8,7 @@ namespace DERHANSEN\SfEventMgt\Tests\Unit\ViewHelpers;
  * LICENSE.txt file that was distributed with this source code.
  */
 
-use DERHANSEN\SfEventMgt\ViewHelpers\Uri\PageViewHelper;
 use Nimut\TestingFramework\TestCase\UnitTestCase;
-use TYPO3\CMS\Core\TimeTracker\TimeTracker;
-use TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext;
-use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * Test case for uri.page viewhelper
