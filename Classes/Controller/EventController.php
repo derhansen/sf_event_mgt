e31bae4ceef40aef66bba59209ee94f44840c7a9
Torben Hansen <derhansen@gmail.com>
2020-04-22 19:58:55 +0200

    [TASK] Remove workaround for TYPO3 forge issue 89434
    Has been fixed with TYPO3 10.4
    
    Refs #750

--- Classes/Controller/EventController.php
+++ Classes/Controller/EventController.php
@@ -437,13 +437,6 @@ class EventController extends AbstractController
      */
     public function registrationAction(Event $event = null)
     {
-        // Temporary workaround for https://forge.typo3.org/issues/89434
-        // @todo: Remove when issue has been fixed
-        if ($event === null && $this->request->getOriginalRequest()->hasArgument('event')) {
-            $uid = $this->request->getOriginalRequest()->getArgument('event');
-            $event = $this->eventRepository->findByUid($uid);
-        }
-
         $event = $this->evaluateSingleEventSetting($event);
         if (is_a($event, Event::class) && $this->settings['registration']['checkPidOfEventRecord']) {
             $event = $this->checkPidOfEventRecord($event);
