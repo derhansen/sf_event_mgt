a97a1c085f3ec66325da1a8bd90bb8e00c40ea33
Torben Hansen <derhansen@gmail.com>
2020-04-23 07:28:24 +0200

    [RELEASE] Release of version 5.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '5.0.0-dev',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.3.0-10.4.99',
+            'typo3' => '10.4.0-10.4.99',
             'php' => '7.2.0-7.3.99',
         ],
         'conflicts' => [],
