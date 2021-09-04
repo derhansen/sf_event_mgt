4e402e429e478c073a855b6e533acbed1c77d76e
Torben Hansen <derhansen@gmail.com>
2021-09-04 20:14:14 +0200

    [TASK] Make CustomNotification DTO more strict
    Refs #922

--- Classes/Domain/Model/Dto/CustomNotification.php
+++ Classes/Domain/Model/Dto/CustomNotification.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Extension "sf_event_mgt" for TYPO3 CMS.
  *
@@ -12,91 +14,52 @@ namespace DERHANSEN\SfEventMgt\Domain\Model\Dto;
 /**
  * Custom notification DTO
  */
-class CustomNotification extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+class CustomNotification
 {
     public const RECIPIENTS_ALL = 0;
     public const RECIPIENTS_CONFIRMED = 1;
     public const RECIPIENTS_UNCONFIRMED = 2;
 
-    /**
-     * @var string
-     */
-    protected $template = '';
+    protected string $template = '';
+    protected int $recipients = self::RECIPIENTS_CONFIRMED;
+    protected string $overwriteSubject = '';
+    protected string $additionalMessage = '';
 
-    /**
-     * @var int
-     */
-    protected $recipients = self::RECIPIENTS_CONFIRMED;
-
-    /**
-     * @var string
-     */
-    protected $overwriteSubject = '';
-
-    /**
-     * @var string
-     */
-    protected $additionalMessage = '';
-
-    /**
-     * @return string
-     */
     public function getTemplate(): string
     {
         return $this->template;
     }
 
-    /**
-     * @param string $template
-     */
     public function setTemplate(string $template): void
     {
         $this->template = $template;
     }
 
-    /**
-     * @return string
-     */
     public function getAdditionalMessage(): string
     {
         return $this->additionalMessage;
     }
 
-    /**
-     * @param string $additionalMessage
-     */
     public function setAdditionalMessage(string $additionalMessage): void
     {
         $this->additionalMessage = $additionalMessage;
     }
 
-    /**
-     * @return int
-     */
     public function getRecipients(): int
     {
         return $this->recipients;
     }
 
-    /**
-     * @param int $recipients
-     */
     public function setRecipients(int $recipients): void
     {
         $this->recipients = $recipients;
     }
 
-    /**
-     * @return string
-     */
     public function getOverwriteSubject(): string
     {
         return $this->overwriteSubject;
     }
 
-    /**
-     * @param string $overwriteSubject
-     */
     public function setOverwriteSubject(string $overwriteSubject): void
     {
         $this->overwriteSubject = $overwriteSubject;
