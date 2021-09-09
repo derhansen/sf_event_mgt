39d84c7ba939cbcd00d60d786de9e794c3e33ffb
Torben Hansen <derhansen@gmail.com>
2021-09-09 15:22:44 +0200

    [TASK] Fixed return type in CleanupCommand

--- Classes/Command/CleanupCommand.php
+++ Classes/Command/CleanupCommand.php
@@ -46,7 +46,7 @@ class CleanupCommand extends Command
      *
      * @param InputInterface $input
      * @param OutputInterface $output
-     * @return int|void|null
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
