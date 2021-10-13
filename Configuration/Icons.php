fcd143071e102a6235154997e0adb8e9c7c770a9
Torben Hansen <derhansen@gmail.com>
2021-10-13 10:57:46 +0200

    [TASK] Remove superfluous code in Icons.php

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -50,26 +50,3 @@ return [
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/hande-expired-registrations.svg',
     ],
 ];
-
-$icons = [
-    'apps-pagetree-folder-contains-events' => 'apps-pagetree-folder-contains-events.svg',
-    'ext-sfeventmgt-wizard' => 'events.svg',
-    'ext-sfeventmgt-registration-unconfirmed' => 'tx_sfeventmgt_domain_model_registration_unconfirmed.svg',
-    'ext-sfeventmgt-registration-confirmed' => 'tx_sfeventmgt_domain_model_registration_confirmed.svg',
-    'ext-sfeventmgt-event' => 'tx_sfeventmgt_domain_model_event.svg',
-    'ext-sfeventmgt-priceoption' => 'tx_sfeventmgt_domain_model_priceoption.svg',
-    'ext-sfeventmgt-organisator' => 'tx_sfeventmgt_domain_model_organisator.svg',
-    'ext-sfeventmgt-location' => 'tx_sfeventmgt_domain_model_location.svg',
-    'ext-sfeventmgt-speaker' => 'tx_sfeventmgt_domain_model_speaker.svg',
-    'ext-sfeventmgt-registration-field' => 'tx_sfeventmgt_domain_model_registration_field.svg',
-    'ext-sfeventmgt-logfile' => 'logfile.svg',
-    'ext-sfeventmgt-action-handle-expired' => 'hande-expired-registrations.svg',
-];
-
-foreach ($icons as $identifier => $path) {
-    $iconRegistry->registerIcon(
-        $identifier,
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        ['source' => 'EXT:sf_event_mgt/Resources/Public/Icons/' . $path]
-    );
-}
