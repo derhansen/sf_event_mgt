b5a074c15ac6a86643b6abd590eef5859fe25b97
Philipp Kitzberger <coding@kitze.net>
2022-02-01 16:18:48 +0100

    Add feature toggle, default=off

--- Configuration/TCA/Overrides/tx_sfeventmgt_domain_model_event.php
+++ Configuration/TCA/Overrides/tx_sfeventmgt_domain_model_event.php
@@ -4,3 +4,9 @@ defined('TYPO3') or die();
 
 // Enable language synchronisation for the category field
 $GLOBALS['TCA']['tx_sfeventmgt_domain_model_event']['columns']['category']['config']['behaviour']['allowLanguageSynchronization'] = true;
+
+$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('sf_event_mgt');
+
+if (empty($extensionConfiguration['contentElementRelation'])) {
+    unset($GLOBALS['TCA']['tx_sfeventmgt_domain_model_event']['columns']['content_elements']);
+}
