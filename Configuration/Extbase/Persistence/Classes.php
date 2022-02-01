d53bbae689c94a7e580cf6252b38388a0eaa4c47
Philipp Kitzberger <coding@kitze.net>
2022-02-01 16:24:14 +0100

    Simplify content model

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -11,19 +11,5 @@ return [
     ],
     \DERHANSEN\SfEventMgt\Domain\Model\Content::class => [
         'tableName' => 'tt_content',
-        'properties' => [
-            'altText' => [
-                'fieldName' => 'altText'
-            ],
-            'titleText' => [
-                'fieldName' => 'titleText'
-            ],
-            'colPos' => [
-                'fieldName' => 'colPos'
-            ],
-            'CType' => [
-                'fieldName' => 'CType'
-            ],
-        ],
     ],
 ];
