154e4af47b0832e6e3ad12727b0b6522f47a4796
Torben Hansen <derhansen@gmail.com>
2022-12-19 07:43:28 +0100

    [TASK] Remove iconfile in TCA of registration
    Refs #1061
    
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- Configuration/TCA/tx_sfeventmgt_domain_model_registration.php
+++ Configuration/TCA/tx_sfeventmgt_domain_model_registration.php
@@ -21,7 +21,6 @@ return [
             'disabled' => 'hidden',
         ],
         'searchFields' => 'firstname,lastname,address,zip,city,phone,email,gender,confirmed,paid,paymentmethod,payment_reference,notes,fe_user,waitlist,',
-        'iconfile' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_registration_unconfirmed.svg',
         'security' => [
             'ignorePageTypeRestriction' => true,
         ],
