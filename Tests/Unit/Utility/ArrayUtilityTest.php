3ddcc9278b77bf4029c034bdd0b29acf608b571e
Torben Hansen <derhansen@gmail.com>
2023-04-06 13:45:38 +0200

    [TASK] Fixed namespace in a test
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- Tests/Unit/Utility/ArrayUtilityTest.php
+++ Tests/Unit/Utility/ArrayUtilityTest.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * LICENSE.txt file that was distributed with this source code.
  */
 
-namespace DERHANSEN\SfEventMgt\Tests\Unit\ArrayUtility;
+namespace DERHANSEN\SfEventMgt\Tests\Unit\Utility;
 
 use DERHANSEN\SfEventMgt\Utility\ArrayUtility;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
