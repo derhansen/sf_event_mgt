52db2d80016be33949c36b9ae6525ac0cb9093a2
Torben Hansen <derhansen@gmail.com>
2023-05-02 16:39:42 +0200

    [RELEASE] Release of version 6.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,11 +20,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '6.3.1',
+    'version' => '6.4.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'php' => '7.4.0-8.1.99',
+            'php' => '7.4.0-8.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
