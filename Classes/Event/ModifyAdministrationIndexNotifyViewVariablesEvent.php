8af9aa532be141b3b0a57e2ff6e16193a3af051b
Steffen Keuper <steffen.keuper@web.de>
2023-07-19 14:51:28 +0200

    [FEATURE] Added PSR-14 events to modify variables for administration module

---
+++ Classes/Event/ModifyAdministrationIndexNotifyViewVariablesEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Extension "sf_event_mgt" for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace DERHANSEN\SfEventMgt\Event;
+
+use DERHANSEN\SfEventMgt\Controller\AdministrationController;
+
+/**
+ * This event is triggered before the administration index notify view is rendered
+ */
+final class ModifyAdministrationIndexNotifyViewVariablesEvent
+{
+    public function __construct(private array $variables, private readonly AdministrationController $administrationController)
+    {
+    }
+
+    public function getVariables(): array
+    {
+        return $this->variables;
+    }
+
+    public function setVariables(array $variables): void
+    {
+        $this->variables = $variables;
+    }
+
+    public function getAdministrationController(): AdministrationController
+    {
+        return $this->administrationController;
+    }
+}
