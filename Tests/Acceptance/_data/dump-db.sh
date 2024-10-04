33dc80d3f72763541d99852f41aedfeb207efb3a
Torben Hansen <derhansen@gmail.com>
2024-10-04 14:51:46 +0200

    [TASK] Update acceptance test configuration
    Closes #1284

--- Tests/Acceptance/_data/dump-db.sh
+++ Tests/Acceptance/_data/dump-db.sh
@@ -4,7 +4,8 @@
 mysql -u$DB_USERNAME -p$DB_PASSWORD_V8 typo3_sfeventmgt_acceptance_v13 -e "DELETE FROM tx_sfeventmgt_domain_model_registration WHERE uid NOT IN(1, 2, 203, 220); DELETE FROM tx_sfeventmgt_domain_model_registration_fieldvalue;" -h127.0.0.1 --port 3406
 mysqldump --no-data -u$DB_USERNAME -p$DB_PASSWORD_V8 -h127.0.0.1 --port 3406 typo3_sfeventmgt_acceptance_v13 > typo3.sql
 mysqldump --no-create-info \
-          --ignore-table=typo3_sfeventmgt_acceptance_v13.sys_log \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_adminpanel_requestcache \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_adminpanel_requestcache_tags \
           --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_hash \
           --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_hash_tags \
           --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_pages \
@@ -13,4 +14,9 @@ mysqldump --no-create-info \
           --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_pagesection_tags \
           --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_rootline \
           --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_rootline_tags \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_treelist \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.sys_history \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.sys_http_report \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.sys_lockedrecords \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.sys_log \
           -u$DB_USERNAME -p$DB_PASSWORD_V8 -h127.0.0.1 --port 3406 typo3_sfeventmgt_acceptance_v13 >> typo3.sql
