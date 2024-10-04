337c1ba813c765e0e42350732530e64ebb45e837
Torben Hansen <derhansen@gmail.com>
2024-10-04 14:47:13 +0200

    [TASK] Update acceptance test configuration
    Closes #1284

--- Tests/Acceptance/_data/dump-db.sh
+++ Tests/Acceptance/_data/dump-db.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Cleans up data from previous runs and dumps database for acceptance tests
-mysql -u$DB_USERNAME -p$DB_PASSWORD typo3_sfeventmgt_acceptance_v12 -e "DELETE FROM tx_sfeventmgt_domain_model_registration WHERE uid NOT IN(1, 2, 203, 220); DELETE FROM tx_sfeventmgt_domain_model_registration_fieldvalue;"
-mysqldump --no-data -u$DB_USERNAME -p$DB_PASSWORD typo3_sfeventmgt_acceptance_v12 > typo3.sql
+mysql -u$DB_USERNAME -p$DB_PASSWORD_V8 typo3_sfeventmgt_acceptance_v13 -e "DELETE FROM tx_sfeventmgt_domain_model_registration WHERE uid NOT IN(1, 2, 203, 220); DELETE FROM tx_sfeventmgt_domain_model_registration_fieldvalue;" -h127.0.0.1 --port 3406
+mysqldump --no-data -u$DB_USERNAME -p$DB_PASSWORD_V8 -h127.0.0.1 --port 3406 typo3_sfeventmgt_acceptance_v13 > typo3.sql
 mysqldump --no-create-info \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.sys_log \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_hash \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_hash_tags \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_pages \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_pages_tags \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_pagesection \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_pagesection_tags \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_rootline \
-          --ignore-table=typo3_sfeventmgt_acceptance_v11.cache_rootline_tags \
-          -u$DB_USERNAME -p$DB_PASSWORD typo3_sfeventmgt_acceptance_v12 >> typo3.sql
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.sys_log \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_hash \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_hash_tags \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_pages \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_pages_tags \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_pagesection \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_pagesection_tags \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_rootline \
+          --ignore-table=typo3_sfeventmgt_acceptance_v13.cache_rootline_tags \
+          -u$DB_USERNAME -p$DB_PASSWORD_V8 -h127.0.0.1 --port 3406 typo3_sfeventmgt_acceptance_v13 >> typo3.sql
