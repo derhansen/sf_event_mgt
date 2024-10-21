5410b3cc3ac10fe48c73e1159d40c1410727b61d
Torben Hansen <derhansen@gmail.com>
2024-10-21 18:29:13 +0200

    [RELEASE] Release of version 8.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,10 +13,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Torben Hansen',
     'author_email' => 'derhansen@gmail.com',
     'state' => 'stable',
-    'version' => '8.0.0-dev',
+    'version' => '8.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.3.0-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
     ],
 ];
