ea83eae59aef9b8facf5c60698cc0c60b09600be
Torben Hansen <derhansen@gmail.com>
2024-12-28 13:22:44 +0100

    [TASK] Use SVG sprite icons for better dark mode support
    Closes #1302

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -10,43 +10,53 @@ return [
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/apps-pagetree-folder-contains-events.svg',
     ],
     'ext-sfeventmgt-registration-unconfirmed' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_registration_unconfirmed.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-registration-unconfirmed',
     ],
     'ext-sfeventmgt-registration-confirmed' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_registration_confirmed.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-registration-confirmed',
     ],
     'ext-sfeventmgt-event' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_event.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-event',
     ],
     'ext-sfeventmgt-priceoption' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_priceoption.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-priceoption',
     ],
     'ext-sfeventmgt-organisator' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_organisator.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-organisator',
     ],
     'ext-sfeventmgt-location' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_location.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-location',
     ],
     'ext-sfeventmgt-speaker' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_speaker.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-speaker',
     ],
     'ext-sfeventmgt-registration-field' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/tx_sfeventmgt_domain_model_registration_field.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-registration-field',
     ],
     'ext-sfeventmgt-logfile' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/logfile.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-logfile',
     ],
     'ext-sfeventmgt-action-handle-expired' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider::class,
         'source' => 'EXT:sf_event_mgt/Resources/Public/Icons/hande-expired-registrations.svg',
+        'sprite' => 'EXT:sf_event_mgt/Resources/Public/Icons/backend-sprites.svg#ext-sfeventmgt-action-handle-expired',
     ],
 ];
