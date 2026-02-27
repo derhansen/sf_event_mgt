25cc94de83837317f374350692708c4220b4fd59
Torben Hansen <derhansen@gmail.com>
2026-02-27 19:11:27 +0100

    [TASK] Use UpgradeWizard classes from ext:core
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- Classes/Updates/PluginToContentElementUpdater.php
+++ Classes/Updates/PluginToContentElementUpdater.php
@@ -11,8 +11,8 @@ declare(strict_types=1);
 
 namespace DERHANSEN\SfEventMgt\Updates;
 
-use TYPO3\CMS\Install\Attribute\UpgradeWizard;
-use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+use TYPO3\CMS\Core\Attribute\UpgradeWizard;
+use TYPO3\CMS\Core\Upgrades\AbstractListTypeToCTypeUpdate;
 
 #[UpgradeWizard('sfEventMgtPluginToContentElementUpdate')]
 class PluginToContentElementUpdater extends AbstractListTypeToCTypeUpdate
