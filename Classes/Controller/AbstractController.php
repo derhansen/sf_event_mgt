5c85020f5a04b696db87cca9ef6e6490b5a2085a
Torben Hansen <derhansen@gmail.com>
2026-08-22 11:39:07 +0200

    [SECURITY] Disallow 'ignoreenablefields' in overwriteDemand

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -34,7 +34,7 @@ use TYPO3\CMS\Frontend\Page\PageInformation;
 
 abstract class AbstractController extends ActionController
 {
-    protected array $ignoredSettingsForOverwriteDemand = ['storagepage', 'orderfieldallowed'];
+    protected array $ignoredSettingsForOverwriteDemand = ['storagepage', 'orderfieldallowed', 'ignoreenablefields'];
 
     protected EventRepository $eventRepository;
     protected RegistrationRepository $registrationRepository;
